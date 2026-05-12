f1e224f3003cd010f2a379e4e1cac3d6d089d40e
1997-bit <132793825+1997-bit@users.noreply.github.com>
2026-05-12 08:18:17 -0500

    feat(core): añadir scaffold inicial del proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto Semestral
+
+## Contabilidad General
---
+++ config/Conexion.php
@@ -0,0 +1,24 @@
+<?php
+class Conexion
+{
+    public static function conectar()
+    {
+        $host = 'localhost';
+        $db   = 'lab5';
+        $user = 'root';
+        $pass = '';
+
+        $pdo = new PDO(
+            "mysql:host=$host;dbname=$db;charset=utf8mb4",
+            $user,
+            $pass,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ]
+        );
+
+        return $pdo;
+    }
+}
